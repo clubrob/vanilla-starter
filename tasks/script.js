8e55d3aa1b328aabb81b07322a4c9dd8ae2ddd55
Rob Lindsey <rob@roblindsey.com>
2018-05-17 19:01:05 -0400

    Moved normalize.css import from SCSS file to gulp task.

--- tasks/script.js
+++ tasks/script.js
@@ -1,10 +1,12 @@
 import gulp from 'gulp';
+import babel from 'gulp-babel';
 import paths from './paths';
 
 const finalDest = paths.scripts.dest;
 
 gulp.task('bundleUp', () =>
   gulp.src(paths.scripts.src)
+    .pipe(babel())
     .pipe(gulp.dest(finalDest)));
 
 const compileScript = gulp.task('compileScript', gulp.series('bundleUp'));
--- tasks/style.js
+++ tasks/style.js
@@ -5,7 +5,7 @@ import paths from './paths';
 const cleanCSS = require('gulp-clean-css');
 const sourcemaps = require('gulp-sourcemaps');
 
-const compileStyle = gulp.task('compileStyle', () =>
+gulp.task('compileSass', () =>
   gulp.src(paths.styles.src)
     .pipe(sourcemaps.init())
     .pipe(sass({
@@ -14,6 +14,12 @@ const compileStyle = gulp.task('compileStyle', () =>
     .pipe(cleanCSS())
     .pipe(sourcemaps.write())
     .pipe(gulp.dest(paths.styles.dest)));
+
+gulp.task('moveNormalize', () =>
+  gulp.src('node_modules/normalize.css/normalize.css')
+    .pipe(gulp.dest(paths.styles.dest)));
+
+const compileStyle = gulp.task('compileStyle', gulp.parallel('compileSass', 'moveNormalize'));
 
 const compileStyleProd = gulp.task('compileStyleProd', () =>
   gulp.src(paths.styles.src)
